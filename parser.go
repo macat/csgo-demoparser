7e67bd6e5466f22d26461de9bb30e580629d0303
Attila Maczak <attila@maczak.hu>
2015-01-24 15:48:18 -0500

    print file size

--- parser.go
+++ parser.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 )
 
 func main() {
-	log.Println("Hello world")
+	log.Println(os.Args)
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileInfo, _ := file.Stat()
+	log.Println(fileInfo.Name())
+	log.Println(fileInfo.Size())
 }
